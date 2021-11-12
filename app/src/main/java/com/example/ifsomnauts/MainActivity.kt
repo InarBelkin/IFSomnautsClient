4c4ec35eb10f46c4611037446cf65da314a9a85c
InarBelkin <Inar2000s@mail.ru>
2021-11-12 18:33:18 +0300

    pp

--- app/src/main/java/com/example/ifsomnauts/MainActivity.kt
+++ app/src/main/java/com/example/ifsomnauts/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.ifsomnauts
 
 import android.os.Bundle
 import android.view.Menu
+import android.widget.TextView
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -9,10 +10,8 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import com.example.ifsomnauts.databinding.ActivityMainBinding
 import com.example.ifsomnauts.models.account.IsAuthDto
-import com.example.ifsomnauts.models.account.userDto
 import com.example.ifsomnauts.repository.AuthRepository
 import com.example.ifsomnauts.repository.connection.NetworkService
 
@@ -20,7 +19,6 @@ import dagger.hilt.android.AndroidEntryPoint
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.util.prefs.Preferences
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -42,34 +40,38 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home,
             ), drawerLayout
         )
 
-        strangeThings();
 
+        strangeThings(binding);
+//        var a = binding.navView.getHeaderView(0);
+//        var tV = a.findViewById<TextView>(R.id.left_panel_username);
+//        tV.text = "cot";
 
         navView.setupWithNavController(navController)
     }
 
-    fun strangeThings(){
+    fun strangeThings(binding: ActivityMainBinding) {
         var pref = getPreferences(MODE_PRIVATE);
-        var aspCookie = pref.getString(getString(R.string.cookieName),"");
+        var aspCookie = pref.getString(getString(R.string.cookieName), "");
         AuthRepository.authCookie.value = aspCookie;
 
-        val cookie:String = AuthRepository.authCookie.value?:"";
+        val cookie: String = AuthRepository.authCookie.value ?: "";
         NetworkService.Instance.account.isAuth(cookie).enqueue(object : Callback<IsAuthDto> {
             override fun onResponse(call: Call<IsAuthDto>, response: Response<IsAuthDto>) {
                 val auth = response.body();
-                if(auth!=null && auth.isAuth){
+                if (auth != null && auth.isAuth) {
                     AuthRepository.user.value = auth.user;
-                }
-                else{
+                } else {
                     AuthRepository.user.value = null;
                 }
             }
+
             override fun onFailure(call: Call<IsAuthDto>, t: Throwable) {
             }
         })
@@ -77,10 +79,22 @@ class MainActivity : AppCompatActivity() {
         AuthRepository.authCookie.observe(this, {
             val prefs = getPreferences(MODE_PRIVATE);
             val ed = prefs.edit()
-            ed.putString(getString(R.string.cookieName),it);
+            ed.putString(getString(R.string.cookieName), it);
             ed.apply();
         });
 
+        AuthRepository.user.observe(this, {
+            val header = binding.navView.getHeaderView(0);
+            val tvName = header.findViewById<TextView>(R.id.left_panel_username);
+            val tvMail = header.findViewById<TextView>(R.id.left_panel_email);
+            if (it != null) {
+                tvName.text = it.name;
+                tvMail.text = it.email;
+            } else {
+                tvName.text = "Авторизуйся!";
+                tvMail.text = "Хотя авторизация сделана ужасно";
+            }
+        });
 
 
     }
